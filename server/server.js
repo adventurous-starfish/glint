e3218c4be1b08b42d54d83a3916a3ed620284b6b
Hack Reactor Students <accounts@hackreactor.com>
2015-03-12 19:57:09 -0700

    Add nodemon, start server.js, and npm install express.

--- server/server.js
+++ server/server.js
@@ -1,2 +1,11 @@
-// configure server
-// configure mongo
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var app = express();
+
+mongoose.connect('mongodb://localhost:27017/glint');
+
+// set up routes
+
+module.exports = app;
