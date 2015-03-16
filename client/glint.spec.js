d952cfc6ead17e764ac0c21bcc232d9d6964360f
annekef <anneke.floor@gmail.com>
2015-03-16 15:42:30 -0700

    Add skeleton tests for new functions

--- client/glint.spec.js
+++ client/glint.spec.js
@@ -16,4 +16,50 @@ describe('MainCtrl', function(){
   it('should have an ideas property on the controller', function(){
     expect(ctrl).to.have.property('ideas');
   });
+
+  it('should have a getIdeas function', function(){
+    //
+  });
+});
+
+describe('SubmitIdeaCtrl', function(){
+  beforeEach(module('glint'));
+  var scope;
+  var ctrl;
+
+  beforeEach(inject(function($rootScope, $controller) {
+    scope = $rootScope.$new();
+    ctrl = $controller('SubmitIdeaCtrl', {
+      self: scope,
+    });
+  }));
+
+  it('should have a submitIdeas function', function(){
+    //
+  });
+});
+
+describe('VotesCtrl', function(){
+  beforeEach(module('glint'));
+  var scope;
+  var ctrl;
+
+  beforeEach(inject(function($rootScope, $controller) {
+    scope = $rootScope.$new();
+    ctrl = $controller('VotesCtrl', {
+      self: scope,
+    });
+  }));
+
+  it('should have an upvote function', function(){
+    //
+  });
+
+  it('should have an downvote function', function(){
+    //
+  });
+
+  it('should have an countVotes function', function(){
+    //
+  });
 });
