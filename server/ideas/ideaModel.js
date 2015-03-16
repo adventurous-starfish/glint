3f504f298c5aa6f88626a98d4b88f7c53075b0be
Kalev Roomann-Kurrik <kalevrk@gmail.com>
2015-03-16 14:52:02 -0700

    (cleanup) Add votes property to Idea schema.

--- server/ideas/ideaModel.js
+++ server/ideas/ideaModel.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 var IdeaSchema = new mongoose.Schema({
     title: String,
     text: String,
+    votes: { type: Number, default: 0 },
     views: { type: Number, default: 0 },
     created_by: { type: String, default: 'anonymous' },
     created_at: { type: Date, default: Date.now },
