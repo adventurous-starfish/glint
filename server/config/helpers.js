f6b0cff34b8ff65e33fc794049d47cbf1fadc2ed
Kalev Roomann-Kurrik <kalevrk@gmail.com>
2015-03-17 15:09:11 -0700

    (testing) Fix error logger and error handler functions in helpers.js.
    - Fix serverSpec.js test to remove any ideas which were stored to the database during testing.

--- server/config/helpers.js
+++ server/config/helpers.js
@@ -1,13 +1,13 @@
 module.exports = {
 
   logErrors: function(err, req, res, next) {
-    console.error(error.stack);
-    next(error);
+    console.error(err.stack);
+    next(err);
   },
 
   handleErrors: function(err, req, res, next) {
     // send client the error message
-    res.send(500, {error: error.message});
+    res.send(500, {error: err.message});
   }
 
 };
