b0c043ca00ee8ea941fe7311b47aff70cb3e1714
annekef <anneke.floor@gmail.com>
2015-03-17 16:29:11 -0700

    (refactor) Merged MainCtrl & SubmitIdeasCtrl into IdeasCtrl
    - This keeps our controllers defined by feature.

--- client/glint.js
+++ client/glint.js
@@ -1,50 +1,7 @@
 var app = angular.module('glint', ['glint.services']);
 
-app.controller('MainCtrl', function(){
+app.controller('IdeasCtrl', function(Ideas){
   var self = this;
-
-  // // mocked up data for now
-  // self.ideas = [
-  //   {
-  //     title: 'Uber for cats',
-  //     text: 'Imagine a world where cats can roam free and never have to walk again! Introducting Uber for cats.',
-  //     votes: 134,
-  //     created_by: 'Super Tom',
-  //     created_at: moment('Mon Mar 16 2015 14:46:59 GMT-0700 (PDT)').fromNow(),
-  //   },
-  //   {
-  //     title: 'Uber for dogs',
-  //     text: 'Imagine a world where dogs can roam free and never have to walk again! Introducting Uber for dogs. The doggiest uber ever. Seriously, we\'re talking pickups for pooches. Cabs for canines. Man\'s best friend will be able to follow him around anywhere. How amazing would that be? You go out for a jog, and then BOOM there\'s little Kibbler, head hanging out the back of a yellow cab, tongue lolling and spittle drooling down the door. Just watching you.',
-  //     votes: 654,
-  //     created_by: 'Fidoman',
-  //     created_at: moment('Sun Mar 15 2015 15:06:59 GMT-0700 (PDT)').fromNow(),
-  //   },
-
-  //   {
-  //     title: 'Uber for meerkats',
-  //     text: 'Imagine a world where meerkats can roam free and never have to walk again! Introducting Uber for meerkats. The doggiest uber ever. Seriously, we\'re talking pickups for pooches. Cabs for canines. Man\'s best friend will be able to follow him around anywhere. How amazing would that be? You go out for a jog, and then BOOM there\'s little Kibbler, head hanging out the back of a yellow cab, tongue lolling and spittle drooling down the door. Just watching you.',
-  //     votes: 512,
-  //     created_by: 'Fidoman',
-  //     created_at: moment('Sun Mar 15 2015 15:06:59 GMT-0700 (PDT)').fromNow(),
-  //   },
-
-  //   {
-  //     title: 'Uber for ferrets',
-  //     text: 'Imagine a world where ferrets can roam free and never have to walk again! Introducting Uber for ferrets. The doggiest uber ever. Seriously, we\'re talking pickups for pooches. Cabs for canines. Man\'s best friend will be able to follow him around anywhere. How amazing would that be? You go out for a jog, and then BOOM there\'s little Kibbler, head hanging out the back of a yellow cab, tongue lolling and spittle drooling down the door. Just watching you.',
-  //     votes: 123,
-  //     created_by: 'Fidoman',
-  //     created_at: moment('Sun Mar 15 2015 15:06:59 GMT-0700 (PDT)').fromNow(),
-  //   },
-
-  //   {
-  //     title: 'Uber for babies',
-  //     text: 'Imagine a world where babies can roam free and never have to walk again! Introducting Uber for babies. The doggiest uber ever. Seriously, we\'re talking pickups for pooches. Cabs for canines. Man\'s best friend will be able to follow him around anywhere. How amazing would that be? You go out for a jog, and then BOOM there\'s little Kibbler, head hanging out the back of a yellow cab, tongue lolling and spittle drooling down the door. Just watching you.',
-  //     votes: 4423,
-  //     created_by: 'Fidoman',
-  //     created_at: moment('Sun Mar 15 2015 15:06:59 GMT-0700 (PDT)').fromNow(),
-  //   }
-  // ];
-
   self.data = { ideas: [] };
 
   // db GET request moves to factory
@@ -52,13 +9,8 @@ app.controller('MainCtrl', function(){
     self.ideas.push(Ideas.getIdeas());
   };
 
-});
-
-app.controller('SubmitIdeaCtrl', function(Ideas){
-  var self = this;
-
   // submitIdea is called when submit button is clicked
-	self.submitIdea = function(){
+  self.submitIdea = function(){
     // escape to handle XSS injection
     var escapedIdea = _.escape(self.ideaTitle);
     var idea = JSON.stringify({
@@ -69,6 +21,7 @@ app.controller('SubmitIdeaCtrl', function(Ideas){
     Ideas.createIdea(idea)
       .then(function(response){
         console.log('createIdea success', response);
+
       })
       .catch(function(error){
         console.error('createIdea error', error);
--- client/glint.spec.js
+++ client/glint.spec.js
@@ -1,34 +1,25 @@
 'use strict';
 
 
-describe('MainCtrl', function(){
+describe('IdeasCtrl', function(){
   beforeEach(module('glint'));
   var scope;
   var ctrl;
 
   beforeEach(inject(function($rootScope, $controller) {
     scope = $rootScope.$new();
-    ctrl = $controller('MainCtrl', {
+    ctrl = $controller('IdeasCtrl', {
       self: scope,
     });
   }));
+
+  it('should have a data property', function(){    
+    expect(ctrl).to.have.property('data');    
+  });
 
   it('should have a getIdeas function', function(){
     expect(ctrl.getIdeas).to.be.a('function');
   });
-});
-
-describe('SubmitIdeaCtrl', function(){
-  beforeEach(module('glint'));
-  var scope;
-  var ctrl;
-
-  beforeEach(inject(function($rootScope, $controller) {
-    scope = $rootScope.$new();
-    ctrl = $controller('SubmitIdeaCtrl', {
-      self: scope,
-    });
-  }));
 
   it('should have a submitIdea function', function(){
     expect(ctrl.submitIdea).to.be.a('function');
