0fe94a6bbd3b593375bb66b6a64e9f5b05f17fff
Kalev Roomann-Kurrik <kalevrk@gmail.com>
2015-03-16 18:25:10 -0700

    (feature) Add ideaController.js file, and npm install q module for using promises with Mongoose commands.

--- server/server.js
+++ server/server.js
@@ -4,6 +4,10 @@ var mongoose = require('mongoose');
 
 var app = express();
 
+// configure Express app to use body-parser module
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+
 // get the correct MongoDB connection depending on the environment
 mongoURI = process.env.CUSTOMCONNSTR_MONGOLAB_URI || 'mongodb://localhost:27017/glint';
 mongoose.connect(mongoURI);
