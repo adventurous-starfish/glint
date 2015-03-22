39c1f9ef7ae23aeeab9abc0ec6fe5738204d8850
millionhari <mikemhari@gmail.com>
2015-03-22 14:43:26 -0700

    Add routes feature

--- client/app/glint.js
+++ client/app/glint.js
@@ -4,8 +4,28 @@ var app = angular.module('glint', [
   'glint.votes',
   'glint.auth',
   'glint.comments',
-  'ngAnimate'
+  'ngAnimate',
+  'ngRoute'
   ])
+
+.config(function($routeProvider){
+	$routeProvider
+		.when('/', {
+        templateUrl: '../index.html',
+        controller: 'IdeasCtrl'
+      })
+    .when('/login', {
+        templateUrl: 'auth/login.html',
+        controller: 'AuthCtrl'
+      })
+    .when('/signup', {
+        templateUrl: 'auth/signup.html',
+        controller: 'AuthCtrl'
+      })
+    .otherwise({
+        redirectTo: '../index.html'
+      });
+})
 
 .filter('moment', function () {
   return function (dateString) {
