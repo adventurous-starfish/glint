fa2ad0c780a2c4fe31fadc477b436f5f518d286a
millionhari <mikemhari@gmail.com>
2015-03-23 15:02:20 -0700

    Delete unnecessary files from landing page

--- server.js
+++
@@ -1,7 +0,0 @@
-var app = require('./server/serverSetup.js').app;
-
-var port = process.env.PORT || 8000;
-
-app.listen(port);
-
-console.log('Server is now listening on port ' + port);
--- server/users/usersRoutes.js
+++
@@ -1,7 +0,0 @@
-var userController = require('./usersController.js');
-
-module.exports = function (app) {
- 
-  app.post('/signin', userController.login);
-  app.post('/signup', userController.signup);
-};
